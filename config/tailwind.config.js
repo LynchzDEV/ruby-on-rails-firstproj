471c0d7b83df2940ec1eb67a25032b2cb3f0f80f
LynchzDEV <opmok48@gmail.com>
2024-11-02 13:22:03 +0700

    Update application layout and todo views

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'red': '#ff0000',
+      }
     },
   },
   plugins: [
